7be551cdf271d9fc8fa33c046264e0b8324bbebf
yaotoshi <yaotoshinakamoto@gmail.com>
2024-04-07 19:28:52 +0700

    done'

--- src/utils/app.ts
+++ src/utils/app.ts
@@ -1,6 +1,6 @@
 import { Frog, FrogConstructorParameters } from 'frog'
 import { neynar } from 'frog/hubs'
-import { isLive } from './dev-tools';
+// import { isLive } from './dev-tools';
 import _ from 'lodash';
 import Nftoshis from 'nftoshis-gating';
 
@@ -12,8 +12,8 @@ const frogAppArgs: FrogConstructorParameters = {
 
 // // this is not working, throw errors:
 // // Error: message is invalid. invalid signer: signer 0x268741c9a443f52192636054ce2b7b746300b3fbba72185a1e23793e9d2f5e81 not found for fid 1
-// frogAppArgs.hub = neynar({ apiKey: process.env.NEYNAR_API_KEY || '' });
-// frogAppArgs.verify = true;
+frogAppArgs.hub = neynar({ apiKey: process.env.NEYNAR_API_KEY || '' });
+frogAppArgs.verify = false;
 
 export const getFrogApp = (opts: FrogConstructorParameters<{State:PollType}> = {}) => new Frog(_.merge(frogAppArgs,opts));
 
