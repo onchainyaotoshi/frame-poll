92281dc936528dbf544ce14fc9a448fb4b9dadb3
Yaotoshi <142854977+onchainyaotoshi@users.noreply.github.com>
2024-04-08 09:21:33 +0000

    The icon was the cause; everything works now.

--- src/controllers/home.tsx
+++ src/controllers/home.tsx
@@ -6,10 +6,8 @@ export default (c: FrameContext, opts:Record<string,any>): TypedResponse<FrameRe
     action: opts.action ? opts.action : undefined,
     image: ListPoll(c, opts),
     intents: [
-        <TextInput placeholder="Fav fruits? ➕ or ID e.g, 1 👁️"/>,
-        <Button value="poll-create-question-submitted" action="/0">Create (➕)</Button>,
-        <Button value="poll-view">View (👁️)</Button>,
-        // <Button value="poll-delete">Delete (❌)</Button>,
-        // <Button value="poll-back">Back</Button>
+        <TextInput placeholder="Enter question e.g, Fav fruits? or ID e.g, 1"/>,
+        <Button value="poll-create-question-submitted" action="/0">Create</Button>,
+        <Button value="poll-view">View</Button>
     ]
 });
--- src/index.tsx
+++ src/index.tsx
@@ -18,7 +18,7 @@ import { fileURLToPath } from 'url';
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export const app = getFrogApp({
-  // browserLocation: '/'
+  browserLocation: '/'
 });
 
 app.use('/*', serveStatic({ root: './public' }))
